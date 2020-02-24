81ee9a77064e56a859678f636f4dbf703078e786
Wavesys <wavesys@localhost.localdomain>
2020-02-24 14:40:01 +0000

    Capitulo 11

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from config import Config
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
