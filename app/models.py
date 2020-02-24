760bd8559ee045cd3c57375910c8c562a1407d7e
Wavesys <wavesys@localhost.localdomain>
2020-02-24 16:52:40 +0000

    Capitulo 13

--- app/models.py
+++ app/models.py
@@ -1,17 +1,19 @@
 from datetime import datetime
-from app import app, db
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
-from app import login
 from hashlib import md5
 from time import time
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import app, db, login
 
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -73,9 +75,11 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
